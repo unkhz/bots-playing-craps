75ac538fc70208ffdd5f9d3624bc804840be1364
Juhani Pelli <juhani.pelli@gmail.com>
2018-02-18 00:29:16 +0000

    Make address reacheable in mobile

--- src/lib/Player/Participant.js
+++ src/lib/Player/Participant.js
@@ -50,50 +50,52 @@ const Participant = ({ player, bets, lastWin, footer, panelBackgroundColor = 'bl
   const derefereredUrl = `http://www.dereferer.org/?${encodeURIComponent(actualUrl)}`;
   return (
     <Box w={1 / 4} m={2} flex="1 1 auto" style={{ minWidth: 50, maxWidth: 150 }}>
-      <Panel width="100%" style={{ backgroundColor: panelBackgroundColor }}>
-        <PanelHeader p={0} style={{ border: '0 solid black' }}>
-          <NavLink p={1} href={derefereredUrl} className="participant">
-            <Code style={{ whiteSpace: 'pre', fontSize: 6 }}>{player.accountId.match(/.{1,14}/g).join('\n')}</Code>
-          </NavLink>
-        </PanelHeader>
-        <Relative>
-          <Absolute style={{ width: '100%' }}>
-            <Subhead
-              m={2}
-              fontSize={32}
-              style={{
-                fontFamily: "'Indie Flower', cursive",
-                textTransform: 'uppercase',
-                textShadow: `0 0 1px ${panelBackgroundColor}, 0 0 10px ${panelBackgroundColor}, 0 0 6px ${panelBackgroundColor}, 0 0 3px ${panelBackgroundColor}`,
+      <a href={derefereredUrl} style={{ textDecoration: 'none', color: 'inherit' }}>
+        <Panel width="100%" style={{ backgroundColor: panelBackgroundColor }}>
+          <PanelHeader p={0} style={{ border: '0 solid black' }}>
+            <NavLink p={1} className="participant">
+              <Code style={{ whiteSpace: 'pre', fontSize: 6 }}>{player.accountId.match(/.{1,14}/g).join('\n')}</Code>
+            </NavLink>
+          </PanelHeader>
+          <Relative>
+            <Absolute style={{ width: '100%' }}>
+              <Subhead
+                m={2}
+                fontSize={32}
+                style={{
+                  fontFamily: "'Indie Flower', cursive",
+                  textTransform: 'uppercase',
+                  textShadow: `0 0 1px ${panelBackgroundColor}, 0 0 10px ${panelBackgroundColor}, 0 0 6px ${panelBackgroundColor}, 0 0 3px ${panelBackgroundColor}`,
+                }}
+              >
+                {player.name}
+              </Subhead>
+            </Absolute>
+            <BackgroundImage
+              ratio={2 / 3}
+              src={`https://robohash.org/${player.accountId}.png`}
+              style={{ height: '3vh' }}
+            />
+            <ChangeObserver observable={player.balance} lifeTime={COLOR_LIFETIME}>
+              {(isChange, prevBalance, nextBalance, timeLeft) => {
+                const isPositive = nextBalance - prevBalance > 0;
+                const life = timeLeft / COLOR_LIFETIME || 0;
+                const alpha = ease(life) / 3 || 0;
+                const backgroundColor =
+                  alpha >= 0
+                    ? isPositive ? `rgba(128, 255, 128, ${alpha})` : `rgba(255, 128, 128, ${alpha})`
+                    : 'transparent';
+                return (
+                  <Text fontSize={14} m={1} style={{ backgroundColor, borderRadius: '4px' }}>
+                    ~ {Math.round(player.balance)} XLM
+                  </Text>
+                );
               }}
-            >
-              {player.name}
-            </Subhead>
-          </Absolute>
-          <BackgroundImage
-            ratio={2 / 3}
-            src={`https://robohash.org/${player.accountId}.png`}
-            style={{ height: '3vh' }}
-          />
-          <ChangeObserver observable={player.balance} lifeTime={COLOR_LIFETIME}>
-            {(isChange, prevBalance, nextBalance, timeLeft) => {
-              const isPositive = nextBalance - prevBalance > 0;
-              const life = timeLeft / COLOR_LIFETIME || 0;
-              const alpha = ease(life) / 3 || 0;
-              const backgroundColor =
-                alpha >= 0
-                  ? isPositive ? `rgba(128, 255, 128, ${alpha})` : `rgba(255, 128, 128, ${alpha})`
-                  : 'transparent';
-              return (
-                <Text fontSize={14} m={1} style={{ backgroundColor, borderRadius: '4px' }}>
-                  ~ {Math.round(player.balance)} XLM
-                </Text>
-              );
-            }}
-          </ChangeObserver>
-        </Relative>
-        <PanelFooter style={{ minHeight: '36px', border: '0 solid black' }}>{footer}</PanelFooter>
-      </Panel>
+            </ChangeObserver>
+          </Relative>
+          <PanelFooter style={{ minHeight: '36px', border: '0 solid black' }}>{footer}</PanelFooter>
+        </Panel>
+      </a>
     </Box>
   );
 };
