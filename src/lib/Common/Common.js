34f66ecfaa7333b5ab08b4d8e4100b189324e493
Juhani Pelli <juhani.pelli@gmail.com>
2018-02-25 00:54:52 +0000

    Reduce noise from Intro

--- src/lib/Common/Common.js
+++ src/lib/Common/Common.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Badge, Text, Relative } from 'rebass';
+import { Badge, Text, Relative, ButtonTransparent } from 'rebass';
+import styled from 'styled-components';
 
 export const RollResult = ({ pass, fail, children, style, ...passedProps }) => (
   <Badge
@@ -23,3 +24,47 @@ export const InlineBlock = ({ children, style, ...passedProps }) => (
     {children}
   </Relative>
 );
+
+const ShowMoreButtonWrapper = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 4em;
+  display: flex;
+  align-items: 'lex-end;
+  background: linear-gradient(
+    180deg,
+    rgba(255,255,255,0) 0%,
+    rgba(255,255,255,0.9) 20%,
+    rgba(255,255,255,0.99) 80%,
+    rgba(255,255,255,1) 100%
+  );
+`;
+
+export class ShowMore extends React.Component {
+  state = { showMore: false };
+  render() {
+    return (
+      <Relative
+        style={{
+          overflow: 'hidden',
+          paddingBottom: this.state.showMore ? 0 : '5em',
+          height: this.state.showMore ? 'auto' : this.props.height,
+        }}
+      >
+        {this.props.children}
+        {this.state.showMore || (
+          <ShowMoreButtonWrapper>
+            <ButtonTransparent
+              onClick={() => this.setState({ showMore: true })}
+              style={{ cursor: 'pointer', margin: '0 auto', flex: '0 0 auto' }}
+            >
+              &lt; Show more &gt;
+            </ButtonTransparent>
+          </ShowMoreButtonWrapper>
+        )}
+      </Relative>
+    );
+  }
+}
--- src/lib/Intro/Intro.js
+++ src/lib/Intro/Intro.js
@@ -1,9 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import Recaptcha from 'react-grecaptcha';
 import store from 'store';
-
-import { Absolute, Relative, Heading } from 'rebass';
-import { RollResult, Strong } from 'lib/Common/Common';
+import { Absolute, Relative, Heading, Text } from 'rebass';
+import { RollResult, Strong, ShowMore } from 'lib/Common/Common';
 
 const STORE_KEY = '9F08E5DE-63D9-417B-B557-4BDE6FFF2103';
 const STORE_VALUE = 'B4538960-2222-458E-A417-6FC5874C5DDB';
@@ -21,46 +20,48 @@ export default class Intro extends Component {
   render() {
     return !this.state.isVerified ? (
       <Fragment>
-        <Heading m={2} fontSize={[18, 24]}>
+        <Heading m={4} fontSize={[18, 24]}>
           Our version of the rules
         </Heading>
-        <Relative style={{ textAlign: 'left', display: 'inline-block', maxWidth: '33em' }}>
-          <ul>
-            <li>
+        <Relative m={2} style={{ textAlign: 'left', display: 'inline-block', maxWidth: '33em' }}>
+          <ShowMore height="11em">
+            <Text fontSize={16}>
               There's a number of bots in the game, each having their own <Strong>Stellar Lumens</Strong> cryptocurrency
-              (XLM) account and some coins.
-            </li>
-            <li>One bot is the dealer, others are players.</li>
-            <li>Player bots bet against the result of dice rolls made by the system.</li>
-            <li>
-              All bets are placed as XLM transactions to the dealer's account. System verifies the transactions are
-              added to the blockchain before rolling.
-            </li>
-            <li>
-              The dice are rolled until a <RollResult pass /> or <RollResult fail /> is reached.
-            </li>
-            <ul>
+              (XLM) account and some coins. One bot is the dealer, others are players. Players bet against the result of
+              dice rolls made by the system. Dealer collects bets and distributes winnings.
+            </Text>
+            <ul m={2}>
               <li>
-                First roll is <RollResult pass /> if the dice produce <Strong>7</Strong> or <Strong>11</Strong>
+                All bets are placed as XLM transactions to the dealer's account. System verifies the transactions are
+                added to the blockchain before rolling.
               </li>
               <li>
-                First roll is <RollResult fail /> if the dice produce <Strong>2</Strong>, <Strong>3</Strong> or{' '}
-                <Strong>12</Strong>
+                The dice are rolled until a <RollResult pass /> or <RollResult fail /> is reached.
               </li>
+              <ul>
+                <li>
+                  First roll is <RollResult pass /> if the dice produce <Strong>7</Strong> or <Strong>11</Strong>
+                </li>
+                <li>
+                  First roll is <RollResult fail /> if the dice produce <Strong>2</Strong>, <Strong>3</Strong> or{' '}
+                  <Strong>12</Strong>
+                </li>
+                <li>
+                  If first roll is undecided the sum of the dice becomes the <Strong>point</Strong>
+                </li>
+                <li>
+                  Further dice rolls are made, until resolution is found. The roll is
+                  <RollResult pass /> if the dice produce the <Strong>point</Strong> or <RollResult fail /> if{' '}
+                  <Strong>7</Strong> is rolled.
+                </li>
+              </ul>
+              <li>Dealer divides the pot proportionally between all the player bots who bet on the correct result.</li>
               <li>
-                If undecided the sum of the dice becomes the <Strong>point</Strong>
-              </li>
-              <li>
-                Further dice rolls are made until the <Strong>point</Strong> is rolled again which equals{' '}
-                <RollResult pass />, or <Strong>7</Strong> is rolled which equals <RollResult fail />
+                System verifies the correct amounts are transferred from the dealer's account to the winners' accounts
+                and concludes the round complete.
               </li>
             </ul>
-            <li>Dealer divides the pot proportionally between all the player bots who bet on the correct result.</li>
-            <li>
-              System verifies the correct amounts are transferred from the dealer's account to the winners' accounts and
-              concludes the round complete.
-            </li>
-          </ul>
+          </ShowMore>
         </Relative>
         <Absolute style={{ bottom: 0, left: 0, right: 0, height: 200 }}>
           <Recaptcha
